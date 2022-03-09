9a292a54a8af2bc00fc4a3b032aecff9177e0b62
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2022-03-10 00:23:02 +0600

    Creating Social App

---
+++ lib/pages/home.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Colors.deepPurple,
+              Colors.teal,
+            ],
+          ),
+        ),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Social App',
+              style: TextStyle(
+                fontSize: 90,
+                fontFamily: 'Signatra',
+                color: Colors.white,
+              ),
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                height: 60,
+                width: 320,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image:
+                          AssetImage('assets/images/google_signin_button.png'),
+                      fit: BoxFit.cover),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
